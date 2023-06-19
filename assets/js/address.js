00c4d46044dd2c9aea0b86a935df17c009ac042f
Cyril Lesaffre <cyril@MacBook-Pro-de-Cyril.local>
2023-06-19 19:12:38 +0200

    add address js

--- assets/js/address.js
+++ assets/js/address.js
@@ -24,33 +24,6 @@ window.addEventListener('load', function(e) {
 
     let selectedBtn = document.querySelectorAll('.selected_btn');
 
-    let getSiblings = function (element, bool) {
-        // for collecting siblings
-        let siblings = []; 
-        // first child of the parent node
-        let sibling  = element.parentNode.parentNode.parentNode.firstChild;
-        // collecting siblings
-        while (sibling) {
-            if (sibling.nodeType === 1 && sibling !== e) {
-                siblings.push(sibling);
-            }
-            sibling = sibling.nextSibling;
-        }
-        if (bool == false) {
-            element.classList.remove('alert-success');
-            return
-        }
-        for (let i = 0; i < siblings.length; i++) {
-            if (bool == true) {
-                if (siblings[i].children[0].children[0] !== element) {
-                    siblings[i].children[0].children[0].classList.remove('alert-success');
-                } else {
-                    siblings[i].children[0].children[0].classList.add('alert-success');
-                }
-            }
-        }
-    };
-
     for (let i = 0; i < selectedBtn.length; i++) {
         selectedBtn[i].addEventListener('click', function() {
             let xhr = new XMLHttpRequest();
@@ -64,16 +37,21 @@ window.addEventListener('load', function(e) {
                 if (data.status === true) {
                     let addressCard = document.getElementById('address_card' + i);
                     if (data.isSelected == true){
-                        getSiblings(addressCard, true);
+                        // reset
+                        let cards = document.querySelectorAll('.card_people');
+                        cards.forEach((object)=> {
+                            object.classList.remove('alert-success');
+                        });
+                        let buttons = document.querySelectorAll('.selected_btn');
+                        buttons.forEach((object) => {
+                            object.innerHTML =  "<i class='fa-solid fa-check fa-2x text-success'></i>";
+                        });
+                        // end reset
+
                         selectedBtn[i].innerHTML = "<i class='fa-solid fa-xmark fa-2x text-danger'></i>";
-                        for (let j = 0; j < selectedBtn.length; j++) {
-                            if (i == j) {
-                                continue;
-                            }
-                            selectedBtn[j].innerHTML = "<i class='fa-solid fa-check fa-2x text-success'></i>";
-                        }
+                        addressCard.classList.add('alert-success');
                     } else {
-                        getSiblings(addressCard, false);
+                        addressCard.classList.remove('alert-success');
                         selectedBtn[i].innerHTML = "<i class='fa-solid fa-check fa-2x text-success'></i>";
                     }
 
