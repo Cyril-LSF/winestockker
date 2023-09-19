c739f77f52ef937a849a7935f2a4750903fc896d
Cyril Lesaffre <cyril.lesaffre@hotmail.fr>
2023-09-19 11:25:40 +0200

    bug fix

--- scripts/env_test_setup.sh
+++ scripts/env_test_setup.sh
@@ -5,5 +5,11 @@ php bin/console doctrine:database:create --env=test
 php bin/console doctrine:schema:drop --force --env=test
 php bin/console doctrine:schema:create --env=test
 php bin/console doctrine:schema:validate --env=test
+php bin/console doctrine:fixtures:load --env=test
+php bin/console doctrine:database:drop --if-exists --force --env=test
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:schema:drop --force --env=test
+php bin/console doctrine:schema:create --env=test
+php bin/console doctrine:schema:validate --env=test
 php bin/phpunit
 echo -e " --> Done\n"
